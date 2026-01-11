b225659d568adf1ae6bec2e70fe83aeb6d87370b
rustamsahu <1029rustam9201@gmail.com>
2026-01-11 19:19:52 +0530

    Add files via upload

---
+++ r1.js
@@ -0,0 +1,34 @@
+let btn=document.querySelector("#additem");
+let tbody=document.querySelector("#tbody");
+let gamnt=document.querySelector("span");
+let print=document.querySelector("#print");
+let qun,prc,tp;
+btn.addEventListener("click",()=>{
+    let row=document.createElement("tr");
+    row.innerHTML=`
+    <td><input type="text"class="name"></td>
+    <td><input type="number"class="quantity" value=1 min=1></td>
+    <td><input type="number"class="price" min=0></td>
+    <td class="total">00</td>`;
+    tbody.append(row);
+    row.addEventListener("input",()=>{
+     let qnt=Number(row.querySelector(".quantity").value);
+     let prc=Number(row.querySelector(".price").value);
+     let ttl=row.querySelector(".total");
+     let sum=qnt*prc;
+     ttl.innerText=sum;
+     calc(); 
+    });
+})
+function calc(){
+    let x=document.querySelectorAll("#tbody tr");
+    let y=0;
+    x.forEach((i)=>{
+          let z=Number(i.querySelector(".total").innerText)||0;
+          y+=z;
+    });
+    gamnt.innerText=y;
+}
+print.addEventListener("click",()=>{
+    window.print();
+});
